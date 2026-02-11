053c4bc8089443b1b4445dbb0fe1f3372765144b
Ribolost <ribolost@hotmail.com>
2026-02-11 10:45:24 -0500

    fix: modify component scanning

--- src/main/java/spring/BackendTechnicalAssessment.java
+++ src/main/java/spring/BackendTechnicalAssessment.java
@@ -3,7 +3,7 @@ package spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"web", "service", "persistence"})
+@SpringBootApplication(scanBasePackages = {"web.*", "service.*", "persistence.*"})
 public class BackendTechnicalAssessment {
     public static void main(String[] args) {
         SpringApplication.run(new Class[]{BackendTechnicalAssessment.class,
